30ca5e2fdec093806a3f6d5d949dcbd53d325644
jordansadie8-alt <jordansadie8@gmail.com>
2026-03-29 18:04:40 -0400

    Add API handler to fetch Substack feed

---
+++ api/index.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const feedUrl = "https://jordansadie.substack.com/feed";
+
+  const response = await fetch(feedUrl);
+  const text = await response.text();
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).json({
+    source: "Substack",
+    feed: feedUrl,
+    content: text
+  });
+}
